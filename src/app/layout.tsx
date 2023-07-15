3d010919776f63c859484cec83338b20df49d6d8
Nico <nico_cartellone@hotmail.com>
2023-07-15 14:17:53 -0300

    feat: fetch to api dolar, map data dolar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={SpaceMono.className}>
         <main className='flex items-center justify-center h-screen bg-[#C5E4E7]'>
-          <div className='flex flex-col flex-1  max-w-screen-lg px-4 mx-auto min-h-[480px]'>
+          <div className='flex flex-col flex-1 max-w-screen-lg px-4 mx-auto min-h-[480px]'>
             <div className='grid flex-1 w-full h-full p-8 bg-white shadow-lg rounded-3xl'>{children}</div>
           </div>
         </main>
